ad41709bb803b0c01396a08686f11a8ee860d789
Kevin Nobel <kevin.nobel@cern.ch>
2024-08-22 17:11:48 +0200

    more verbose array checking

--- inc/util.h
+++ inc/util.h
@@ -19,11 +19,19 @@ T arrayProduct(T a[], size_t n)
 }
 
 template <typename T>
-b8 checkArray(const usize n, const T *observed, const T *expected, const f64 maxError = 0.000000000001) {
-    for (usize i = 0; i < n; ++i)
-        if (fabsl((observed[i] - expected[i]) / expected[i]) > maxError)
-            return false;
-    return true;
+b8 checkArray(const usize n, const T *observed, const T *expected, const f64 maxError = 0.000000000001, const isize maxReports = 25, std::ostream &output = std::cerr) {
+    isize reports = maxReports >= 0 ? 0 : -1;
+    usize errors = 0;
+    for (usize i = 0; i < n; ++i) {
+        const f64 error = fabsl((observed[i] - expected[i]) / expected[i]);
+        if (error > maxError) {
+            ++errors;
+            if (reports >= 0 && reports++ < maxReports) {
+                output << "Error at index " << i << " : " << observed[i] << " != " << expected[i] << " (" << error << ")" << std::endl;
+            }
+        }
+    }
+    return errors == 0;
 }
 
 template <typename T>

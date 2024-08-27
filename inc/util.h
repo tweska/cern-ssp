4ee4b492e8c87bbcdfc67410aad958b12304acac
Kevin Nobel <kevin.nobel@cern.ch>
2024-08-27 12:00:32 +0200

    add average array check and array max error functions

--- inc/util.h
+++ inc/util.h
@@ -35,6 +35,40 @@ b8 checkArray(const usize n, const T *observed, const T *expected, const f64 max
 }
 
 template <typename T>
+b8 checkArrayAvg(const usize n, const usize k, const T *observed, const T *expected, const f64 maxError = 0.000000000001, const isize maxReports = 25, std::ostream &output = std::cerr) {
+    const auto nAvg = n - (k - 1);
+    T *observedAvg = new T[nAvg];
+    T *expectedAvg = new T[nAvg];
+    for (usize i = 0; i < nAvg; ++i) {
+        observedAvg[i] = 0.0;
+        expectedAvg[i] = 0.0;
+        for (usize j = 0; j < k; ++j) {
+            observedAvg[i] += observed[i + j];
+            expectedAvg[i] += expected[i + j];
+        }
+    }
+    auto result = checkArray(nAvg, observedAvg, expectedAvg, maxError, maxReports, output);
+    delete[] observedAvg;
+    delete[] expectedAvg;
+    return result;
+}
+
+template <typename T>
+void arrayMaxError(T *maxError, T *oValue, T *eValue, const usize n, const T *observed, const T *expected) {
+    assert(n > 0);
+    *maxError = fabsl((observed[0] - expected[0]) / expected[0]);
+    *oValue = observed[0];
+    *eValue = expected[0];
+    for (usize i = 1; i < n; ++i) {
+        if (const f64 error = fabsl((observed[i] - expected[i]) / expected[i]); error > *maxError) {
+            *maxError = error;
+            *oValue = observed[i];
+            *eValue = expected[i];
+        }
+    }
+}
+
+template <typename T>
 void printArray(T* array, usize n, std::ostream &output = std::cout)
 {
     output << "[ ";
